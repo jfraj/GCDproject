3069f320d2eb241640dd5d0e9cbc1deac99961c0
jfraj <rajotte.jeanfrancois@gmail.com>
2015-01-18 13:43:31 -0500

    Added column names

--- run_analysis.R
+++ run_analysis.R
@@ -4,17 +4,22 @@ run_analysis<-function(){
     #For now, I limit the number of row
     maxrows <- 5#for fast testing
     #maxrows<- -1#Will be ignored if negative according to docs
+    
+    ##This will be used for the column names
+    df_names<-read.table("UCI HAR Dataset/features.txt", col.names=c("id", "feature"))
+    
+    
     ftrain<-"UCI HAR Dataset/train/X_train.txt"
-    df_train<-read.table(ftrain, nrows=maxrows)
-    dim(df_train)
+    df_train<-read.table(ftrain, nrows=maxrows, col.names=as.vector(df_names$feature))
     #Now the test data
     ftest<-"UCI HAR Dataset/test/X_test.txt"
-    df_test<-read.table(ftrain, nrows=maxrows)
+    df_test<-read.table(ftrain, nrows=maxrows, col.names=as.vector(df_names$feature))
     df_full<- rbind(df_test, df_train)
     
     #2.Extracts only the measurements on the mean and standard deviation for each measurement.
-    ##Make sure there are no field with meand and sd in each rows...
-    df_sum<-apply(df_full, 2, mean)
-    df_sum<-rbind(df_sum, apply(df_full, 2, sd))
-    row.names(df_sum)<-c("mean", "sd")
+    ##The the measuremens for the mean and stand. dev. have mean and std in their name
+    ##Lets create a vector of names using the features.txt file
+    #df_sum<-apply(df_full, 2, mean)
+    #df_sum<-rbind(df_sum, apply(df_full, 2, sd))
+    #row.names(df_sum)<-c("mean", "sd")
 }
