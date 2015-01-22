616bae66a052e955918319cbe07274678d134e46
jfraj <rajotte.jeanfrancois@gmail.com>
2015-01-22 15:21:27 -0500

    Now has maxrows as argument with default -1 (all) to test interactively

--- run_analysis.R
+++ run_analysis.R
@@ -1,8 +1,8 @@
-run_analysis<-function(){
+run_analysis<-function(maxrows=-1){
     #1.Merges the training and the test sets to create one data set.
     #Let's get the train data
     #For now, I limit the number of row
-    maxrows <- 5#for fast testing
+    #maxrows <- 5#for fast testing
     #maxrows<- -1#Will be ignored if negative according to docs
     
     ##This will be used for the column names
@@ -27,4 +27,5 @@ run_analysis<-function(){
     df_names[grep("Mean|mean|std", df_names$feature),]##Did not find other combitnations
     vsummarynames<-grep("Mean|mean|std",colnames(df_full))
     dfsummary<-df_full[,vsummarynames]
+    print(dim(dfsummary))
 }
