40a5ea1e275374a13b48ef536f10dee4632d231e
jfraj <rajotte.jeanfrancois@gmail.com>
2015-01-23 14:58:21 -0500

    Better names (removed dots, brakets and dash)

--- run_analysis.R
+++ run_analysis.R
@@ -7,19 +7,30 @@ run_analysis<-function(maxrows=-1){
     
     ##This will be used for the column names
     df_names<-read.table("UCI HAR Dataset/features.txt", col.names=c("id", "feature"))
-    
+    cnames<-as.vector(df_names$feature)
+    ##Improving the names
+    #cnames <- sub("()", "", cnames, fixed = TRUE)
+    cnames <- gsub("(", "", cnames, fixed = TRUE)
+    cnames <- gsub(")", "", cnames, fixed = TRUE)
+    cnames <- gsub(".", "", cnames, fixed = TRUE)
+    cnames <- gsub("-", "", cnames, fixed = TRUE)
+    cnames <- gsub(",", "to", cnames, fixed = TRUE)
+    #print(cnames)
+    #cat ("Press [enter] to continue")
+    #line <- readline()
     
     ftrain<-"UCI HAR Dataset/train/X_train.txt"
-    df_train<-read.table(ftrain, nrows=maxrows, col.names=as.vector(df_names$feature))
+    df_train<-read.table(ftrain, nrows=maxrows, col.names=cnames)
     print(dim(df_train))
     
     #Now the test data
     ftest<-"UCI HAR Dataset/test/X_test.txt"
-    df_test<-read.table(ftest, nrows=maxrows, col.names=as.vector(df_names$feature))
+    df_test<-read.table(ftest, nrows=maxrows, col.names=cnames)
     print(dim(df_test))
     
     #merge
     df_full<- rbind(df_test, df_train)
+    print(names(df_full))
     
     #2.Extracts only the measurements on the mean and standard deviation for each measurement.
     ##The the measuremens for the mean and stand. dev. have mean and std in their name
@@ -29,7 +40,8 @@ run_analysis<-function(maxrows=-1){
     #row.names(df_sum)<-c("mean", "sd")
     #
     #To find column names with mean in it use df_names[grep("mean", df_names$feature),]
-    df_names[grep("Mean|mean|std", df_names$feature),]##Did not find other combitnations
+    #df_names[grep("Mean|mean|std", df_names$feature),]##Did not find other combitnations
+    df_names[grep("Mean|mean|std", cnames),]##Did not find other combitnations
     vsummarynames<-grep("Mean|mean|std",colnames(df_full))
     dfsummary<-df_full[,vsummarynames]
     print(dim(dfsummary))
@@ -42,6 +54,7 @@ run_analysis<-function(maxrows=-1){
     print(class(df_avg))
     print(df_avg[1])
     #print(names(df_avg))
-    print(df_avg["angle.Y.gravityMean."])
-    print(df_avg["angle.X.gravityMean."])
+    #print(df_avg["angle.Y.gravityMean."])
+    #print(df_avg["angle.X.gravityMean."])
+    #print(cnames)
 }
