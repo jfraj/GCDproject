948ed5fe945b0fa5056b5967aba8f611b5d462f4
jfraj <rajotte.jeanfrancois@gmail.com>
2015-01-24 10:14:07 -0500

    Changing the merge part.  All following parts need to be changed too

--- run_analysis.R
+++ run_analysis.R
@@ -16,21 +16,45 @@ run_analysis<-function(maxrows=-1){
     cnames <- gsub("-", "", cnames, fixed = TRUE)
     cnames <- gsub(",", "to", cnames, fixed = TRUE)
     #print(cnames)
-    #cat ("Press [enter] to continue")
-    #line <- readline()
     
+    #train
     ftrain<-"UCI HAR Dataset/train/X_train.txt"
-    df_train<-read.table(ftrain, nrows=maxrows, col.names=cnames)
-    print(dim(df_train))
+    #df_train<-read.table(ftrain, nrows=maxrows, col.names=cnames)
+    df_train<-read.table(ftrain, nrows=maxrows)
+    flabels<-"UCI HAR Dataset/train/y_train.txt"
+    df_train_labels<-read.table(flabels, nrows=maxrows)
+    ftrain_subjects<-"UCI HAR Dataset/train/subject_train.txt"
+    df_train_subjects<-read.table(ftrain_subjects, nrows=maxrows)
     
-    #Now the test data
+    
+    #test
     ftest<-"UCI HAR Dataset/test/X_test.txt"
-    df_test<-read.table(ftest, nrows=maxrows, col.names=cnames)
-    print(dim(df_test))
+    df_test<-read.table(ftest, nrows=maxrows)
+    ftest_labels<-"UCI HAR Dataset/test/y_test.txt"
+    df_test_labels<-read.table(ftest_labels, nrows=maxrows)
+    ftest_subjects<-"UCI HAR Dataset/test/subject_test.txt"
+    df_test_subjects<-read.table(ftest_subjects, nrows=maxrows)
     
+    
+    print(dim(df_train))
+    print(dim(df_train_labels))
+    print(dim(df_test))
+    print(dim(df_test_labels))
+        
     #merge
     df_full<- rbind(df_test, df_train)
-    print(names(df_full))
+    df_labels<- rbind(df_test_labels, df_train_labels)
+    df_subjects<- rbind(df_test_subjects, df_train_subjects)
+    print(dim(df_full))
+    print(dim(df_labels))
+    print(dim(df_subjects))    
+    cat ("Press [enter] to continue")
+    line <- readline()
+
+    cat ("Press [enter] to continue")
+    line <- readline()
+    
+    
     
     #2.Extracts only the measurements on the mean and standard deviation for each measurement.
     ##The the measuremens for the mean and stand. dev. have mean and std in their name
@@ -52,8 +76,9 @@ run_analysis<-function(maxrows=-1){
     #   with the average of each variable for each activity and each subject.
     df_avg<-colMeans(dfsummary)
     print(class(df_avg))
-    print(df_avg[1])
-    #print(names(df_avg))
+    print(df_avg[1:5])
+    print(dim(df_avg))
+    print(names(df_avg))
     #print(df_avg["angle.Y.gravityMean."])
     #print(df_avg["angle.X.gravityMean."])
     #print(cnames)
